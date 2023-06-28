978a4a2ae377c8bf07e61731e46d48f510a4fb92
Nishanth Kumar K S <134582426+nish049@users.noreply.github.com>
2023-06-28 20:30:47 +0530

    Create internet-speed-tester.py

---
+++ internet-speed-tester.py
@@ -0,0 +1,8 @@
+
+import speedtest
+
+wifi=speedtest.Speedtest()
+up=wifi.upload()/1048576
+down=wifi.download()/1048576
+print(f"Wifi Upload speed is {up},mbps")
+print(f"Wifi Download speed is {down},mbps")
